293afa4d00b66cff334d814e70f367463617c72a
Gustavo <maxwelgustavo@gmail.com>
2024-05-06 21:10:57 -0300

    Thread Camareira implementada, mas ainda não testada

--- Hotel/src/Camareira.java
+++ Hotel/src/Camareira.java
@@ -1,3 +1,45 @@
-public class Camareira {
-    
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Camareira extends Thread {
+  private static int count = 0;
+  private List<Quarto> quartos;
+  private volatile boolean ativo;
+
+  public Camareira(List<Quarto> quartos) {
+    super("Camareira " + ++count);
+    this.quartos = quartos;
+    this.ativo = true;
+  }
+
+  public void pararDeLimpar() {
+    this.ativo = false;
+  }
+
+  @Override
+  public void run() {
+    while (this.ativo) {
+      boolean todosQuartosLimpos = true;
+      for (Quarto quarto : quartos) {
+        synchronized (quarto) {
+          try {
+            quarto.limpar();
+          } catch (Qocupado e) {
+            todosQuartosLimpos = false;
+            continue;
+          }
+        }
+      }
+      if (todosQuartosLimpos) {
+        this.parar();
+      }
+      try {
+        Thread.sleep(11100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
