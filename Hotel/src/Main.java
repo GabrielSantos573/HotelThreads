0e31e625f9768bd80f40358bbeea390546cfb3c6
Gustavo <maxwelgustavo@gmail.com>
2024-05-06 22:33:20 -0300

    Classe Main, criada mas ainda não testada

--- Hotel/src/Main.java
+++ Hotel/src/Main.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        List<Quarto> quartos = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            quartos.add(new Quarto(i));
+        }
+
+        Queue<Hospede> filaDeEspera = new LinkedList<>();
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            threads.add(new Hospede(quartos));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Camareira(quartos));
+        }
+
+        for (int i = 0; i < 5; i++) {
+            threads.add(new Recepcionista(quartos, filaDeEspera));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
     }
 }
-
-    //main
